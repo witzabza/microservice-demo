28841378055d11a8a2fa433c52bf6cadf28a2538
witzabza <chonlawitth@gmail.com>
2019-06-09 09:24:22 +0000

    add config kubectl

--- script/install.sh
+++ script/install.sh
@@ -3,3 +3,5 @@
 curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
 chmod +x ./kubectl
 sudo mv ./kubectl /usr/local/bin/kubectl
+kubectl --kubeconfig ./script/config version
+#kubectl apply -f ./deploy/kubernetes/complete-demo.yaml
